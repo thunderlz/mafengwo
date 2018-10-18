12e6176f671c2b382a53866054c7cd96716a703f
thunderlz <thunderlz@163.com>
2018-10-18 21:50:53 +0800

    设置requests编码为utf-8

--- mafengwo.py
+++ mafengwo.py
@@ -63,10 +63,10 @@ for p in range(100,1000):
 
                 if not os.path.exists(path+'目的地/{}/{}/'.format(target,title)):
                     os.mkdir(path+'目的地/{}/{}/'.format(target,title))
-                    with open(path+'目的地/{}/{}/{}.html'.format(target,title,title), 'wt') as f:
+                    with open(path+'目的地/{}/{}/{}.html'.format(target,title,title), 'wb') as f:
                         # pass
                         rq.encoding='utf-8'
-                        print(rq.text,file=f)
+                        print(rq.text.encode('utf-8'),file=f)
                         # print(bs.find_all('img'))
 
                     for img in bs.find_all('img'):
